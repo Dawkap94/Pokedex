e4c2d355fb8ffcc3979427457982663ca5b185df
Dawid Kapusta <dawidkapusta67@gmail.com>
2022-08-31 12:51:59 +0200

    added opposite types

--- pokedex.py
+++ pokedex.py
@@ -6,6 +6,7 @@ import requests
 import pyttsx3
 import pypokedex
 from io import BytesIO
+from opposite_types import opposite_types
 
 
 def SpeakText(text_variable):
@@ -44,7 +45,7 @@ pokemon_information.config(font=("Roboto", 15))
 pokemon_moves.pack(pady=10, padx=10)
 
 pokemon_types = tk.Label(window)
-pokemon_types.config(font=("Roboto", 20))
+pokemon_types.config(font=("Roboto", 15))
 pokemon_types.pack(padx=10, pady=10)
 
 
@@ -59,8 +60,8 @@ def load_pokemon():
 
     pokemon_information.config(text=f"{pokemon.dex} - {pokemon.name.capitalize()} \n "
                                     f"Atk: {pokemon.base_stats.attack}, Def: {pokemon.base_stats.defense}, Speed: {pokemon.base_stats.speed}")
-
-    pokemon_types.config(text=f"Type: " + ', '.join(pokemon.types).capitalize())
+    enemies = str([opposite_types[elem] for elem in pokemon.types]).replace("[", "").replace("]", "").replace("'", "")
+    pokemon_types.config(text=f"Type: " + ', '.join(pokemon.types) + "\n Vulnerability: " + enemies )
 
     pkmn_moves = {move.level: move.name.replace("-", " ") for move in pokemon.moves["red-blue"] if move.level}
     sorted_moves = sorted(pkmn_moves)
@@ -77,11 +78,11 @@ label_id_name = tk.Label(window, text="Insert ID(1-898) or Name")
 label_id_name.config(font=("Arial", 20))
 label_id_name.pack(padx=10, pady=10)
 
-text_id_name = tk.Text(window, height=1)
+text_id_name = tk.Text(window, height=1, background="azure")
 text_id_name.config(font=("Arial", 20))
 text_id_name.pack(pady=10, padx=10)
 
-btn_load = tk.Button(window, text="Load Pokemon!", command=load_pokemon)
+btn_load = tk.Button(window, text="Load Pokemon!", command=load_pokemon, background="chartreuse4")
 btn_load.config(font=("Arial", 20))
 btn_load.pack(padx=10, pady=10)
 
